9a0d5d1cf3e1d90aefbaf0e7275c784e37b6c351
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-03 22:07:52 +0200

    Setting up nav layout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,14 @@
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <nav>
+      <div className="container nav-items">
+        <h1>Where in the world?</h1>
+        <div className="theme-toggler">
+          <i className="fa-regular fa-moon"></i>
+          <p>Dark Mode</p>
+        </div>
+      </div>
+    </nav>
+  );
 }
 export default Navigation;
