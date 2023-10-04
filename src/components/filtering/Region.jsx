216b439ce38b49a68fb83af65968da8aae3dfc83
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-04 21:23:06 +0200

    jsx layout created for filter components

--- src/components/filtering/Region.jsx
+++ src/components/filtering/Region.jsx
@@ -2,6 +2,20 @@
 
 function Region() {
   // const { filterCategory, setFilterCategory } = useDataHandler();
-  return <div>Filter</div>;
+  return (
+    <div className="region">
+      <button className="region-btn">
+        Filter by Region
+        <i className="fa-solid fa-chevron-down"></i>
+      </button>
+      <ul className="region-list">
+        <li>Africa</li>
+        <li>America</li>
+        <li>Asia</li>
+        <li>Europe</li>
+        <li>Oceania</li>
+      </ul>
+    </div>
+  );
 }
 export default Region;
--- src/components/filtering/Searchbar.jsx
+++ src/components/filtering/Searchbar.jsx
@@ -2,6 +2,15 @@
 
 function Searchbar() {
   // const { searchValue, setSearchValue } = useDataHandler();
-  return <div>Searchbar</div>;
+  return (
+    <div className="searchbar">
+      <i className="fa-solid fa-magnifying-glass"></i>
+      <input
+        type="text"
+        name="searchbar"
+        placeholder="Search for a country..."
+      />
+    </div>
+  );
 }
 export default Searchbar;
