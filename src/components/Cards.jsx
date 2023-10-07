378123c512f9175f022306569745ef9a2908c13a
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-07 15:30:49 +0200

    Card stylings added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,82 @@
-import { useDataHandler } from "../context/handleDataContext";
+// import { useDataHandler } from "../context/handleDataContext";
 
 function Card() {
-  const { filterCategory, searchValue } = useDataHandler();
+  // const { filterCategory, searchValue } = useDataHandler();
 
   return (
-    <section className="cards">
-      {filterCategory} | {searchValue}
+    <section className="card-container">
+      <div className="card">
+        <img
+          src="https://flagcdn.com/w320/no.png"
+          alt="test"
+        />
+        <h2>Norway</h2>
+        <div className="card-facts">
+          <p>
+            <span>Population:</span> 5,000,000
+          </p>
+          <p>
+            <span>Region:</span> Europe
+          </p>
+          <p>
+            <span>Capital:</span> Oslo
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://flagcdn.com/w320/no.png"
+          alt="test"
+        />
+        <h2>Norway</h2>
+        <div className="card-facts">
+          <p>
+            <span>Population:</span> 5,000,000
+          </p>
+          <p>
+            <span>Region:</span> Europe
+          </p>
+          <p>
+            <span>Capital:</span> Oslo
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://flagcdn.com/w320/no.png"
+          alt="test"
+        />
+        <h2>Norway</h2>
+        <div className="card-facts">
+          <p>
+            <span>Population:</span> 5,000,000
+          </p>
+          <p>
+            <span>Region:</span> Europe
+          </p>
+          <p>
+            <span>Capital:</span> Oslo
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://flagcdn.com/w320/no.png"
+          alt="test"
+        />
+        <h2>Norway</h2>
+        <div className="card-facts">
+          <p>
+            <span>Population:</span> 5,000,000
+          </p>
+          <p>
+            <span>Region:</span> Europe
+          </p>
+          <p>
+            <span>Capital:</span> Oslo
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
