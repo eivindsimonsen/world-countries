0561b303f41064534c10eaff1e81054085d3b0d4
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-07 13:33:17 +0200

    Fetching the search input, and storing it in useContext

--- src/components/filtering/Searchbar.jsx
+++ src/components/filtering/Searchbar.jsx
@@ -1,7 +1,12 @@
-// import { useDataHandler } from "../../context/handleDataContext";
+import { useDataHandler } from "../../context/handleDataContext";
 
 function Searchbar() {
-  // const { searchValue, setSearchValue } = useDataHandler();
+  const { setSearchValue } = useDataHandler();
+
+  const getValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className="searchbar">
       <i className="fa-solid fa-magnifying-glass"></i>
@@ -9,6 +14,7 @@ function Searchbar() {
         type="text"
         name="searchbar"
         placeholder="Search for a country..."
+        onChange={getValue}
       />
     </div>
   );
