a91476ab994cc16796b6d2dd189ddc805854c8ba
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-07 13:25:46 +0200

    Animation added to list, and setting value of clicked region to state

--- src/components/filtering/Region.jsx
+++ src/components/filtering/Region.jsx
@@ -1,19 +1,32 @@
-// import { useDataHandler } from "../../context/handleDataContext";
+import { useDataHandler } from "../../context/handleDataContext";
+import { useState } from "react";
 
 function Region() {
-  // const { filterCategory, setFilterCategory } = useDataHandler();
+  const { setFilterCategory } = useDataHandler();
+  const [active, setActive] = useState(false);
+
+  const toggleList = () => {
+    setActive(!active);
+  };
+
+  const setRegion = (event) => {
+    setFilterCategory(event.target.innerText);
+  };
+
   return (
     <div className="region">
-      <button className="region-btn">
+      <button
+        onClick={toggleList}
+        className="region-btn">
         Filter by Region
         <i className="fa-solid fa-chevron-down"></i>
       </button>
-      <ul className="region-list">
-        <li>Africa</li>
-        <li>America</li>
-        <li>Asia</li>
-        <li>Europe</li>
-        <li>Oceania</li>
+      <ul className={"region-list " + (active ? "active" : "")}>
+        <li onClick={setRegion}>Africa</li>
+        <li onClick={setRegion}>America</li>
+        <li onClick={setRegion}>Asia</li>
+        <li onClick={setRegion}>Europe</li>
+        <li onClick={setRegion}>Oceania</li>
       </ul>
     </div>
   );
