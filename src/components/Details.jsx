630cce669493ee13edfc515d5402830765966413
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-08 15:01:04 +0200

    Details page button added and styled correctly to themes

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,4 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Details() {
-  return <div>Details</div>;
+  return (
+    <main className="container">
+      <Link
+        to="/"
+        className="cta">
+        <i className="fa-solid fa-arrow-left"></i>Back
+      </Link>
+    </main>
+  );
 }
 export default Details;
