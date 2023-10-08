5ea1fd5e1c9ef13484a318727f9c9c4162626370
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-08 17:47:11 +0200

    Fetching data from the json file, and displaying on screen

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,32 +1,59 @@
-// import { useDataHandler } from "../context/handleDataContext";
-
+import { useState, useEffect } from "react";
+import { useDataHandler } from "../context/handleDataContext";
 import { Link } from "react-router-dom";
 
 function Card() {
-  // const { filterCategory, searchValue } = useDataHandler();
+  const { filterCategory, searchValue } = useDataHandler();
+  const [data, setData] = useState(null);
+  console.log(filterCategory);
+  console.log(searchValue);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../../data/data.json");
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching or parsing data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
 
   return (
     <section className="card-container">
-      <Link
-        to="details/23"
-        className="card ">
-        <img
-          src="https://flagcdn.com/w320/no.png"
-          alt="test"
-        />
-        <h2>Norway</h2>
-        <div className="card-facts">
-          <p>
-            <span>Population:</span> 5,000,000
-          </p>
-          <p>
-            <span>Region:</span> Europe
-          </p>
-          <p>
-            <span>Capital:</span> Oslo
-          </p>
-        </div>
-      </Link>
+      {data &&
+        data.map((country, index) => (
+          <Link
+            to={`details/${country.numericCode}`}
+            className="card "
+            key={index}>
+            <img
+              src={country.flags.png}
+              alt="test"
+            />
+            <h2>{country.name}</h2>
+            <div className="card-facts">
+              <p>
+                <span>Population:</span> {country.population}
+              </p>
+              <p>
+                <span>Region:</span> {country.region}
+              </p>
+              <p>
+                <span>Capital:</span> {country.capital}
+              </p>
+            </div>
+          </Link>
+        ))}
     </section>
   );
 }
